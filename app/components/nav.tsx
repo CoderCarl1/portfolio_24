f1999079c8488b9768bc8b7526d5c40dd04e06da
carl davidson <carl.davidson@hotmail.com>
2024-01-23 10:36:54 +1000

    basic nav styling

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,15 +1,17 @@
 import { Link } from "@remix-run/react";
+import logo from '~/images/CD_Dev_Logo_200x200.png';
 
 
 
 export default function Nav() {
 
   return (
-    <header>
-      <nav>
-        <Link to="#about">About</Link>
-        <Link to="#contact">Contact</Link>
-        <Link to="#projects">Projects</Link>
+    <header className="[ header ][ flex justify-between align-center ]">
+      <Link className="logo" to="/"><img src={logo} alt="Home" className='hero__image'/></Link>
+      <nav className="[ nav ][ even-columns basis:40 ]">
+        <Link className="[ button ]" to="#about">About</Link>
+        <Link className="[ button ]" to="#projects">Projects</Link>
+        <Link className="[ button ]" to="#contact">Contact</Link>
       </nav>
     </header>
   )
