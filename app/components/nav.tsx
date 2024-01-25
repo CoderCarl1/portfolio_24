64316a3b745efb1fb1dd7187fd5394a474e803b8
carl davidson <carl.davidson@hotmail.com>
2024-01-25 10:41:58 +1000

    remove text decoration on logo

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -7,7 +7,7 @@ export default function Nav() {
 
   return (
     <header className="[ header ][ flex justify:between align:center ]">
-      <Link className="[ logo ][ flex ]" to="/">
+      <Link className="[ logo ][ flex decoration:none ]" to="/">
         <img src={logo} alt="Home" className='[ logo__image ]'/>
         <h1 className="[ logo__heading ][ fs-300 ]">Coder Carl</h1>
       </Link>
