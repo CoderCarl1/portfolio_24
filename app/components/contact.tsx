4dae935824c0d855638d0f6c31f2ab963833b5de
carl davidson <carl.davidson@hotmail.com>
2024-01-25 10:42:36 +1000

    begin form for contact

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,15 +1,52 @@
-import {Section} from "@components";
+import { Section } from "@components";
+import { ActionFunctionArgs, redirect } from "@remix-run/node";
+import { Form, useNavigation } from "@remix-run/react";
+
 
 
 export default function Contact() {
+  const navigation = useNavigation();
+
+  const isSubmitting =
+  navigation.formAction === "/?index";
 
   return (
-    <Section id="contact" className="TEST">
-      <h2>Contact Carl Davidson</h2>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ratione minus temporibus expedita fuga, laboriosam voluptatum minima aliquid ab voluptate.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ratione minus temporibus expedita fuga, laboriosam voluptatum minima aliquid ab voluptate.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ratione minus temporibus expedita fuga, laboriosam voluptatum minima aliquid ab voluptate.</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum ratione minus temporibus expedita fuga, laboriosam voluptatum minima aliquid ab voluptate.</p>
+    <Section id="contact" className="[ container-medium ]">
+      <h2 id="contact_header">Contact Carl Davidson</h2>
+
+      <Form action="/?index" method="post">
+        <fieldset disabled={isSubmitting} className="[ contact__form ]">
+          <legend className="[ contact__form--legend ][ fs-600 ]">Contact Me</legend>
+
+          <div className="[ even-columns-row gap:16 ]">
+            <div className="[ input-wrapper floating ]">
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                className="[ input ]"
+              />
+              <label>Name</label>
+              
+            </div>
+            <div className="[ input-wrapper floating ]">
+              <input
+                type="text"
+                name="email"
+                placeholder="Email"
+                className="[ input ]"
+              />
+              <label>Email</label>
+              {/* <p className="visually-hidden invalid-field" id="emailHint">Please enter a valid email address</p> */}
+            </div>
+          </div>
+
+          <textarea name="message" id="message" cols={30} rows={10} placeholder="your message..."></textarea>
+          {/* <div aria-live="assertive" id="formResult" className="formResult"></div> */}
+          <button className="[ button ][ text:center ]" type="submit">Send</button>
+        </fieldset>
+
+      </Form>
     </Section>
   )
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,20 @@
 import Home from '~/pages/Home'
-import type { MetaFunction } from "@remix-run/node";
-
+import {ActionFunctionArgs, redirect, type MetaFunction } from "@remix-run/node";
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
+
+// Contact Form Submission
+export async function action({request}: ActionFunctionArgs){
+
+  const body = await request.formData();
+  const values = Object.fromEntries(body);
+  // TODO: set up email
+  console.log("formData ", values)
+    return redirect(`/#contact`);
+}
 
 export default Home;
