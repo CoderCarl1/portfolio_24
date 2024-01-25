a99a89ecc8942455f438e8bfc19a0d398d74d65b
carl davidson <carl.davidson@hotmail.com>
2024-01-25 10:41:42 +1000

    UPDATE Section to handle optional classnames

--- app/components/section.tsx
+++ app/components/section.tsx
@@ -7,8 +7,9 @@ type SectionProps = {
 } & React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
 
 export default function Section({ children, className = "", ...props }: SectionProps) {
+  const additionalClasses = className ? `[ ${className} ]` : '';
   return (
-    <section className={joinClasses('section flow:32', className)} {...props}>
+    <section className={joinClasses('[ section flow:32 ]', additionalClasses)} {...props}>
       {children}
     </section>
   )
