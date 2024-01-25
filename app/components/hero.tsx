19671152eaa2b327b980e81da4ffee4f72da099d
carl davidson <carl.davidson@hotmail.com>
2024-01-25 10:41:05 +1000

    update hero images

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,24 +1,33 @@
 import { Link } from '@remix-run/react';
-import hero_image from '~/images/HeroImage.png';
+import hero_image_800x800 from '~/images/heroImage_800x800.webp';
+import hero_image_720x720 from '~/images/heroImage_720x720.webp';
+import hero_image_400x400 from '~/images/heroImage_400x400.webp';
+
 import Blob from '~/images/blob';
 export default function Hero() {
 
   return (
     <div className="[ hero ][ container-medium flow even-columns-row ][ align:center ]">
       
-      <div className='[ hero__text ][ text-left fs-800 flow:16 ]'>
-        <p>Hi 👋! <span className="text-left">I'm Carl a front-end focused web developer.</span></p>
+      <div className='[ hero__text ][ text:left fs-800 flow:16 ]'>
+        <p>Hi 👋! <span className="text:left">I'm Carl a front-end focused web developer.</span></p>
         <Link to="#contact" className="[ button ][ fs-500 ]">Contact Me</Link>
       </div>
       
       <div
         className='[ hero__image ][ margin-x:auto ]'
       >
-        <img
-          src={hero_image}
+        <picture 
           className="[ hero__image--photo ]"
+        >
+          <source media="(max-width: 999px)" srcSet={hero_image_400x400}/>
+          <source media="(min-width: 1000px)" srcSet={hero_image_720x720}/>
+
+        <img
+          src={hero_image_720x720}
           alt="Carl Davidson smiling with a coloured background"
         />
+        </picture>
         <div
           className="[ hero__image--blobs ]"
         >
