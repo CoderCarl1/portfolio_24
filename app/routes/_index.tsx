65d8d3f59c0d6b166cf4c9b69333d25e9cf72bf8
carl davidson <carl.davidson@hotmail.com>
2024-01-26 10:26:40 +1000

    update metatags and favicon

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,28 @@
 import Home from '~/pages/Home'
 import {ActionFunctionArgs, redirect, type MetaFunction } from "@remix-run/node";
 import sendEmail from '~/[server]/email.server';
+import DEVLOGO from '~/images/CD_Dev_Logo_200x200.png';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Coder Carl's Portfolio" },
+    { name: "description", content: "A personal blog / portfolio for Coder Carl." },
+    {name: "og:title", content: "Coder Carl's Portfolio"},
+    {name: "og:url", content: "https://www.codercarl.dev"},
+    {name: "og:description", content: "A personal blog / portfolio for Coder Carl."},
+    {name: "og:image", content: DEVLOGO},
+    {name: "og:type", content: "website"},
+    {name: "og:locale", content: "en-AU"},
+
   ];
 };
 
 // Contact Form Submission
 export async function action({request}: ActionFunctionArgs){
   const formData = await request.formData();
-  await sendEmail(formData);
+  const {status} = await sendEmail(formData);
+  // add errors as params to redirect if missing
+  // name, email, message 
   return redirect(`/#contact`);
 }
 
